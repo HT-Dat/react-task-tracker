df18c721e25d286b7ddb631b13dcdd31d4f82233
hotiendat661 <hotiendat661@gmail.com>
2021-07-31 18:31:13 +0700

    creating a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Header from "./component/Header";
 function App() {
   const name = false;
   const x = true;
   return (
     <div className="container">
+      <Header />
       <h1>Hello From React</h1>
       <h2>Hello {x == true ? "Yes" : "No"}</h2>
+
     </div>
   );
 }
