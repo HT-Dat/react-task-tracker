7897f8e9270127f3ce91579eb8a134a5f68e2a6b
hotiendat661 <hotiendat661@gmail.com>
2021-08-02 15:23:44 +0700

    Toggle reminder & conditional styling

--- src/App.js
+++ src/App.js
@@ -9,20 +9,28 @@ function App() {
       text: "Doctors Appointment",
       day: "Feb 5th at 2:30pm",
       reminder: true,
-    },{
-        id: 2,
-        text: "Meeting at school",
-        day: "Feb 6th at 1:30pm",
-        reminder: true,
-      },
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
   ]);
   const deleteTask = (id) => {
-    setTask(tasks.filter(tasks => tasks.id !== id))
-  }
+    setTask(tasks.filter((tasks) => tasks.id !== id));
+  };
+  const toggleReminder = (id) => {
+    setTask(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder}:task));
+  };
   return (
     <div className="container">
       <Header />
-      {tasks.length>0? (<Tasks tasks={tasks} onDelete={deleteTask}/>) : 'No task to show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        "No task to show"
+      )}
     </div>
   );
 }
