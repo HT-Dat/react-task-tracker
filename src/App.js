550b74a907574920cd165aebaa3d356b54c6917f
hotiendat661 <hotiendat661@gmail.com>
2021-08-03 17:19:14 +0700

    Add task to json server

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ function App() {
     return data;
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
     setTask(tasks.filter((tasks) => tasks.id !== id));
   };
 
@@ -32,10 +33,20 @@ function App() {
     );
   };
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000 + 1);
-    const newTask = { id, ...task };
-    setTask([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTask([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000 + 1);
+    // const newTask = { id, ...task };
+    // setTask([...tasks, newTask]);
   };
   return (
     <div className="container">
